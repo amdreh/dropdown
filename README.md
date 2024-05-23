209f844e913b127d6fae716648a8581dd54f66f9
amdreh <amdreh@gmail.com>
2024-05-23 20:00:06 -0300

    Mudança de cor do fundo funcionando

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Título
 Descrição
+
+
--- index.js
+++ index.js
@@ -1,5 +1,45 @@
 // TESTE 
 // $(document).ready(function(){
 //     // Altera a cor de fundo para preto
-//     $('body').css('background-color', 'black');
+    // $('body').css('background-color', 'black');
 // });
+
+$(document).ready(function() {
+    $('#color1').click(function() {
+        $(".bg").css("display", "none");
+        $(".bg2").css("display", "block");
+        var bgColor = $(this).text().trim();
+        $('.container-fluid').css('background-color', bgColor);
+        $('.bg').css('color', bgColor);
+    });
+
+
+    $('#color2').click(function() {
+        $(".bg").css("display", "none");
+        $(".bg2").css("display", "block");
+        var bgColor = $(this).text().trim();
+        $('.container-fluid').css('background-color', bgColor);
+        $('.bg').css('color', bgColor);
+    });
+
+    $('#color3').click(function() {
+        $(".bg").css("display", "none");
+        $(".bg2").css("display", "block");
+        var bgColor = $(this).text().trim();
+        $('.container-fluid').css('background-color', bgColor);
+        $('.bg').css('color', bgColor);
+    });
+
+    $('#color4').click(function() {
+        $(".bg").css("display", "none");
+        $(".bg2").css("display", "block");
+        var bgColor = $(this).text().trim();
+        $('.container-fluid').css('background-color', bgColor);
+        $('.bg').css('color', bgColor);
+    });
+
+
+
+    
+
+});
