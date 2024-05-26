a0f8e49aaecaef838fab8cccd912f63a6d4434ad
amdreh <amdreh@gmail.com>
2024-05-26 12:10:59 -0300

    Tudo ok, só falta alinhar o menu

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
     // $('body').css('background-color', 'black');
 // });
 
+var cont = 0;
+
+
 $(document).ready(function() {
     $('#color1').click(function() {
 
@@ -30,8 +33,32 @@ $(document).ready(function() {
         $('.container-fluid').css('background-color', bgColor);
     });
 
+    function getBackgroundColor(element) {
+        return window.getComputedStyle(element).backgroundColor;
+    }
 
+    $('.btnMude').click(function() {
+        var bodyBackgroundColor = getBackgroundColor(document.body);
 
-    
-
+        if (cont == 4) {
+            cont=0;
+        }
+        if (cont == 3) {
+            document.querySelector('.container-fluid').style.backgroundColor = 'rgb(0,0,0)';
+            cont++;
+        }
+        if (cont == 2) {
+            document.querySelector('.container-fluid').style.backgroundColor = 'rgb(133,145,116)';
+            cont++;
+        }
+        if (cont == 1) {
+            document.querySelector('.container-fluid').style.backgroundColor = 'rgb(244,198,86)';
+            cont++;
+        }
+        if (cont == 0) {
+            document.querySelector('.container-fluid').style.backgroundColor = 'rgb(255,247,234)';
+            cont++;
+            
+        }
+    });
 });
